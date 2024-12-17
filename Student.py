dc089da19059129465435f47ba39edfb058a4101
Nitin Kumar <nitin16072005@gmail.com>
2024-12-17 15:37:49 +0530

    changes in methods name

--- Student.py
+++ Student.py
@@ -1,23 +1,23 @@
 class Student:
     def __init__(self, studentId, firstName, lastName, email, phoneNo):
-        self.studentId = StudentId
-        self.firstName = FirstName
-        self.lastName = LastName
-        self.email = Email
-        self.phoneNo = PhoneNo
+        self.StudentId = studentId
+        self.FirstName = firstName
+        self.LastName = lastName
+        self.Email = email
+        self.PhoneNo = phoneNo
 
-    def get_student_info(self):
-        return f"Student ID: {self.studentId}, First Name: {self.firstName}, Last Name: {self.lastName}, Email: {self.email}, Phone No: {self.phoneNo}"
+    def GetStudentInfo(self):
+        return f"Student ID: {self.StudentId}, First Name: {self.FirstName}, Last Name: {self.NastName}, Email: {self.Email}, Phone No: {self.PhoneNo}"
 
-    def update_details(self, firstName=None, lastName=None, email=None, phoneNo=None):
+    def UpdateDetails(self, firstName=None, lastName=None, email=None, phoneNo=None):
         if firstName:
-            self.firstName = firstName
+            self.FirstName = firstName
         if lastName:
-            self.lastName = lastName
+            self.LastName = lastName
         if email:
-            self.email = email
+            self.Email = email
         if phoneNo:
-            self.phoneNo = phoneNo
-        return f"Student {studentId} details updated successfully."
+            self.PhoneNo = phoneNo
+        return f"Student {self.StudentId} details updated successfully."
 
 
