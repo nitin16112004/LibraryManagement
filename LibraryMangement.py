ac82e9fffa761dbba8d8a635fd0f985038f76799
Nitin Kumar <nitin16072005@gmail.com>
2024-12-20 11:58:16 +0530

    add some  features

--- LibraryMangement.py
+++ LibraryMangement.py
@@ -10,8 +10,10 @@ while True:
     print("2. Add Book")
     print("3. Get Student Info")
     print("4. Get Book Info")
-    print("5. View Library Summary")
-    print("6. Exit")
+    print("5. Delete Student")
+    print("6. Delete Book")
+    print("7. View Library Summary")
+    print("8. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -52,6 +54,23 @@ while True:
             print(book)
 
     elif choice == "5":
+        studentId = int(input("Enter Student ID to delete: "))
+        for student in students:
+            if student.StudentId == studentId:
+                print(f"Student Id {studentId} has been removed")
+            else:
+                print(f"Student ID {studentId} not found")
+
+    elif choice == "6":
+        bookId = int(input("Enter Book ID to delete: "))
+        for book in books:
+            if book.BookId == bookId:
+                print(f"Book with ID {bookId} has been removed")
+            else:
+                print(f"Book ID {bookId} not found")
+
+
+    elif choice == "7":
         if students:
             print("\nStudents in Library:")
             for student in students:
@@ -66,7 +85,7 @@ while True:
         else:
             print("\nNo books registered.")
 
-    elif choice == "6":
+    elif choice == "8":
         print("Exiting the system. Goodbye!")
         break
 
